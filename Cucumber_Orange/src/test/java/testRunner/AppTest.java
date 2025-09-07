6dc43d28e8beeebc6655f705ec400d1e3e3e7ea9
Ashok <ashoknsyl@gmail.com>
2025-09-07 14:00:57 +0530

    modified testRunner

--- Cucumber_Orange/src/test/java/testRunner/AppTest.java
+++ Cucumber_Orange/src/test/java/testRunner/AppTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(dryRun=false, features = {"featurefiles/login.feature"},
 glue = {"stepDefinition"},
-plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json",
+plugin = {"pretty", "html:target/cucumber-reports/report.html", "json:target/cucumber-reports/report.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},monochrome=true)
 public class AppTest extends AbstractTestNGCucumberTests {
 
